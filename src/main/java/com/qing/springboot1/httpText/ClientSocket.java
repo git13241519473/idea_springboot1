d1491270a5831868767896ee27a9d04fa766cdd4
liangzhiqing <liangzhiqingkenan@163.com>
2019-01-17 19:09:28 +0800

    socket exit

--- src/main/java/com/qing/springboot1/httpText/ClientSocket.java
+++ src/main/java/com/qing/springboot1/httpText/ClientSocket.java
@@ -9,46 +9,38 @@ public class ClientSocket {
 
     public static void main(String args[]) {
         try {
-            //向本机的4700端口发出客户请求
-            Socket socket = new Socket("127.0.0.1",4700);
+            Socket socket = new Socket("127.0.0.1",4700);   //向本机的4700端口发出客户请求
+
+            //由Socket对象得到输入流，并构造相应的BufferedReader对象
+            //由Socket对象得到输出流，并构造PrintWriter对象
+            BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter os = new PrintWriter(socket.getOutputStream());
 
             //由系统标准输入设备构造BufferedReader对象
-            BufferedReader sin = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader sreader = new BufferedReader(new InputStreamReader(System.in));
 
-            //由Socket对象得到输出流，并构造PrintWriter对象
-            PrintWriter os = new PrintWriter(socket.getOutputStream());
-            //由Socket对象得到输入流，并构造相应的BufferedReader对象
-            BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String readline = sreader.readLine(); //从系统标准输入读入一字符串
 
-            String readline = sin.readLine(); //从系统标准输入读入一字符串
+            //若从标准输入读入的字符串为 "bye"则停止循环
+            while(true){
+                os.println(readline);   //将从系统标准输入读入的字符串输出到Server
+                os.flush();     //刷新输出流，使Server马上收到该字符串
 
-                //若从标准输入读入的字符串为 "bye"则停止循环
-            while(!readline.equals("exit")){
-                //将从系统标准输入读入的字符串输出到Server
-                os.println(readline);
-                os.flush(); //刷新输出流，使Server马上收到该字符串
+                System.out.println("Client:"+readline); //在系统标准输出上打印读入的字符串
 
-
-                System.out.println("Client:"+readline);
-
-                //在系统标准输出上打印读入的字符串
-                System.out.println("Server:"+is.readLine());
-
-                //从Server读入一字符串，并打印到标准输出上
-                readline = sin.readLine(); //从系统标准输入读入一字符串
-
-            } //继续循环
-
+                if("exit".equals(readline)){
+                    break;
+                }
+                System.out.println("Server:"+is.readLine());    //从Server读入一字符串，并打印到标准输出上
+                //继续循环
+                readline = sreader.readLine(); //从系统标准输入读入一字符串
+            }
             os.close(); //关闭Socket输出流
-
             is.close(); //关闭Socket输入流
-
             socket.close(); //关闭Socket
 
         } catch(Exception e) {
-
             System.out.println("Error"+e); //出错，则打印出错信息
-
         }
 
     }
