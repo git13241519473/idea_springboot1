d55385edb2d203780ae6e7ff1984a02c6b6c1560
liangzhiqing <liangzhiqingkenan@163.com>
2019-01-21 18:09:10 +0800

    添加redis操作

--- src/main/java/com/qing/springboot1/controller/UserController.java
+++ src/main/java/com/qing/springboot1/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.qing.springboot1.controller;
 
+import com.qing.springboot1.utils.redisTest.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private RedisUtil redisUtil;
 
     @RequestMapping("hello")
     public String sayHello(String name) {
@@ -15,12 +21,12 @@ public class UserController {
         return "hello! " + name;
     }
 
-    @RequestMapping("hello2")
-    public String sayHello2(String name) {
-        if(!(name != null && !"".equals(name))){
-            name = "";
-        }
-        return "hello2 ! " + name;
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        boolean aSet = redisUtil.set("a", "123", 10);
+        long aLong = redisUtil.getExpire("a");
+        return aLong + "";
     }
 
     @RequestMapping("test")
