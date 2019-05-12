434430df0137699d955b940271e8ca8a556e61d1
git13241519473 <liangzhiqingkenan@163.com>
2019-05-12 09:08:13 +0800

    lzq

--- src/main/java/com/qing/springboot1/baseNumType/ExchangeInteger.java
+++ src/main/java/com/qing/springboot1/baseNumType/ExchangeInteger.java
@@ -21,11 +21,20 @@ public class ExchangeInteger {
 
     }
 
+    //第一种
     private static void swap(Integer a, Integer b) throws NoSuchFieldException, IllegalAccessException {
         int temp = a;  // 所以最后修改为：统一使用 int 类型的值，做
         Field valueFiled = Integer.class.getDeclaredField("value");
         valueFiled.setAccessible(true);
         valueFiled.setInt(a, b);
         valueFiled.setInt(b, temp);
+    }
+    //第二种
+    private static void swap2(Integer a, Integer b) throws NoSuchFieldException, IllegalAccessException {
+        Integer temp = new Integer(a.intValue()); //把 temp 重新存储一个内存空间
+        Field valueFiled = Integer.class.getDeclaredField("value");
+        valueFiled.setAccessible(true);
+        valueFiled.set(a, b);
+        valueFiled.set(b, temp);
     }
 }
