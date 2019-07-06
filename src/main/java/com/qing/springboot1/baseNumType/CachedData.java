906c5b4a5b90aa1dc58c4c522ce9fc1ee38821a4
git13241519473 <liangzhiqingkenan@163.com>
2019-07-06 14:24:03 +0800

    lzq

---
+++ src/main/java/com/qing/springboot1/baseNumType/CachedData.java
@@ -0,0 +1,35 @@
+package com.qing.springboot1.baseNumType;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+class CachedData {
+    Object data;
+    volatile boolean cacheValid;
+    final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+
+    public void processCachedData() {
+        rwl.readLock().lock();
+        if (!cacheValid) {
+            // Must release read lock before acquiring write lock
+            rwl.readLock().unlock();
+            rwl.writeLock().lock();
+            try {
+                // Recheck state because another thread might have,acquired write lock and changed state before we did.
+                if (!cacheValid) {
+                    data = null; //...
+                    cacheValid = true;
+                }
+                // 在释放写锁之前通过获取读锁降级写锁(注意此时还没有释放写锁)
+                rwl.readLock().lock();
+            } finally {
+                rwl.writeLock().unlock(); // 释放写锁而此时已经持有读锁
+            }
+        }
+
+        try {
+            //use(data);
+        } finally {
+            rwl.readLock().unlock();
+        }
+    }
+}
