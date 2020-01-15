4a7b63d43d692f827023c0d2b300f416bacf8b98
git13241519473 <liangzhiqingkenan@163.com>
2020-01-15 10:13:39 +0800

    好久不见，hello!

--- src/main/java/com/qing/springboot1/baseNumType/RandomNum.java
+++ src/main/java/com/qing/springboot1/baseNumType/RandomNum.java
@@ -2,12 +2,12 @@ package com.qing.springboot1.baseNumType;
 
 public class RandomNum {
     public static void main(String[] args) {
-        for(int n = 0; n < 30; n++){
+        for(int n = 0; n < 100; n++){
             System.out.print(randomNum(10, 50) + "\t");
         }
     }
 
-
+    //取值范围：[min, max]
     public static int randomNum(int min, int max){
         return (int)(min + Math.random() * (max - min + 1));
     }
